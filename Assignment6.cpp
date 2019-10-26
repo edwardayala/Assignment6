54071e9cf1b54451426436e05678dad8f5c44659
edwardayala <eaayala@txwes.edu>
2019-10-26 01:51:07 -0500

    Added first map and print, prints in order for some reason

--- Assignment6.cpp
+++ Assignment6.cpp
@@ -8,3 +8,32 @@
 // Add function to sort remaining data in each collection
 // Print sorted data
 
+// C++ Associative Array = Map
+// C++ Dynamic Array = Vector (many other implementations)
+
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    map<string, int> cppMap {
+            {"Bob", 34},
+            {"John", 26},
+            {"Steve", 30},
+            {"Frank", 43},
+            {"Peter", 52},
+            {"Albert", 20},
+            {"Jim", 65},
+            {"Alan", 85},
+            {"James", 41},
+            {"Bill", 36}
+    };
+
+    map<string, int>::iterator i;
+    cout << "C++ Map:" << endl;
+    for (auto i = cppMap.begin(); i != cppMap.end(); i++)
+        cout << i->first << " is " << i->second << " years old.\n";
+}
