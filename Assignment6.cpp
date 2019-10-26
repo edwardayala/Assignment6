6bd3163237de2f2977caf3b99932487ede654189
edwardayala <eaayala@txwes.edu>
2019-10-26 01:04:40 -0500

    Add C++

---
+++ Assignment6.cpp
@@ -0,0 +1,10 @@
+// Assignment 6: Exploring Languages with Associative Arrays, Dynamic Lists, and Iteration
+// Edward Ayala | Braxton Brown | Suvaik Patel
+// Create Map & Dynamic List
+// Write function to iterate over list and output contents (Print function)
+// Print after adding 10 items to each collection
+// Print again after adding another 10 items to each collection
+// Print a final time after deleted 5 specific items from each collection
+// Add function to sort remaining data in each collection
+// Print sorted data
+
